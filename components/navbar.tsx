c2fda0f171e9ce5a9da2780eebd78042182c1e34
Zachary Hartman <105947716+pnwdevzac@users.noreply.github.com>
2024-03-11 16:23:07 -0700

    updated link calls for mobile burger menu nav

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -87,27 +87,31 @@ export const Navbar = () => {
 			</NavbarContent>
 
 			<NavbarMenu>
-				
-				<div className="mx-4 mt-2 flex flex-col gap-2">
-					{siteConfig.navMenuItems.map((item, index) => (
-						<NavbarMenuItem key={`${item}-${index}`}>
-							<Link
-								color={
-									index === 2
-										? "primary"
-										: index === siteConfig.navMenuItems.length - 1
-										? "danger"
-										: "foreground"
-								}
-								href="#"
-								size="lg"
-							>
-								{item.label}
-							</Link>
-						</NavbarMenuItem>
-					))}
-				</div>
-			</NavbarMenu>
+  <div className="mx-4 mt-2 flex flex-col gap-2">
+    {siteConfig.navMenuItems.map((item, index) => (
+      <NavbarMenuItem key={`${item}-${index}`}>
+        <NextLink href={item.href}>
+          <a
+            className={clsx(
+              linkStyles({ color: "foreground" }),
+              "data-[active=true]:text-primary data-[active=true]:font-medium"
+            )}
+            color={
+              index === 2
+                ? "primary"
+                : index === siteConfig.navMenuItems.length - 1
+                ? "danger"
+                : "foreground"
+            }
+          >
+            {item.label}
+          </a>
+        </NextLink>
+      </NavbarMenuItem>
+    ))}
+  </div>
+</NavbarMenu>
+
 		</NextUINavbar>
 	);
 };
